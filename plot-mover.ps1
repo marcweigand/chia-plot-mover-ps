c06d367c31887e32e6fa6957897985b424e409ae
marcweigand <114650921+marcweigand@users.noreply.github.com>
2023-05-22 16:25:43 +0200

    0.0.4

--- plot-mover.ps1
+++ plot-mover.ps1
@@ -4,6 +4,14 @@ function Get-DriveFreeSpace($drive){
     return $drive.FreeSpace / 1GB
 }
 
+function Test-PathAvailability($path){
+    return Test-Path -Path $path
+}
+
+function IsEnoughSpace($path, $size){
+    return (Get-DriveFreeSpace($path) -gt $size)
+}
+
 $sourceDir = 'D:\'
 $destinationDrives = '\\SERVER\SHARE1', '\\SERVER\SHARE2'
 
@@ -12,38 +20,35 @@ while($true){
     Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Checking for new .plot files."
     foreach($plotFile in $plotFiles){
         $mostSpace = $destinationDrives | Sort-Object {Get-DriveFreeSpace($_)} -Descending | Select-Object -First 1
-        if($null -eq $mostSpace){
-            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- No available drives. Will check again later."
-            break
-        }
-        $fileSize = (Get-Item $sourceFile).length / 1GB
-        if($fileSize -gt (Get-DriveFreeSpace($mostSpace))){
-            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Not enough free space on drive: $mostSpace. Will check again later."
-            break
-        }
         Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Most free space on drive: $mostSpace with " (Get-DriveFreeSpace($mostSpace)) "GB free."
-        Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Beginning to move $plotFile to $mostSpace"
-        $sourceFile = Join-Path -Path $sourceDir -ChildPath $plotFile
-        $destinationFile = Join-Path -Path $mostSpace -ChildPath $plotFile
-        $transferred = 0
-        $progress = @{
-            Activity = "Moving $plotFile to $mostSpace"
-            Status = "$transferred of $fileSize transferred"
-            PercentComplete = 0
-        }
-        Write-Progress @progress
-        Copy-Item -Path $sourceFile -Destination $destinationFile
-        if(Test-Path $destinationFile){
-            Remove-Item -Path $sourceFile
-            $transferred = $fileSize
-            $progress.Status = "$transferred of $fileSize transferred"
-            $progress.PercentComplete = 100
+        $fileSize = $plotFile.Length / 1GB
+        if(Test-PathAvailability($mostSpace) -and IsEnoughSpace($mostSpace, $fileSize)){
+            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Beginning to move $plotFile to $mostSpace"
+            $destinationFile = Join-Path -Path $mostSpace -ChildPath $plotFile.Name
+            $transferred = 0
+            $progress = @{
+                Activity = "Moving $plotFile to $mostSpace"
+                Status = "$transferred of $fileSize transferred"
+                PercentComplete = 0
+            }
             Write-Progress @progress
-            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Finished moving $plotFile to $mostSpace"
-            Write-Progress -Activity $progress.Activity -Completed
-        }
-        else{
-            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Failed to move $plotFile to $mostSpace"
+            try{
+                Copy-Item -Path $plotFile.FullName -Destination $destinationFile -ErrorAction Stop
+                Remove-Item -Path $plotFile.FullName
+                $transferred = $fileSize
+                $progress.Status = "$transferred of $fileSize transferred"
+                $progress.PercentComplete = 100
+                Write-Progress @progress
+                Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Finished moving $plotFile to $mostSpace"
+                Write-Progress -Activity $progress.Activity -Completed
+            }
+            catch{
+                Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Failed to move $plotFile to $mostSpace due to error: $_"
+                Start-Sleep -Seconds 600
+            }
+        }else{
+            Write-Host (Get-Date -Format "yyyy-MM-dd HH:mm:ss") "- Cannot move $plotFile to $mostSpace due to insufficient space or path unavailability"
+            Start-Sleep -Seconds 600
         }
     }
     Start-Sleep -Seconds 600
